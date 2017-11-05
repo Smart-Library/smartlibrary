0c710928336bacd0caea7aad68eab33fd97b8991
Dominik Schmidtlein <dominik.schmidtlein@gmail.com>
2017-11-04 20:53:22 -0400

    added tests for models

--- test/models/desk_test.rb
+++ test/models/desk_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 
 class DeskTest < ActiveSupport::TestCase
-<<<<<<< HEAD
   def setup
     @desk1 = desks :desk1
   end
@@ -29,9 +28,4 @@ class DeskTest < ActiveSupport::TestCase
   test 'belongs to grouping1' do
     assert_equal(groupings(:grouping1), @desk1.grouping)
   end
-=======
-  # test "the truth" do
-  #   assert true
-  # end
->>>>>>> Added models for desk and groupings
 end
