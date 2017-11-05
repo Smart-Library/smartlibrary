f7f389aa3936647f34dbc0d8b4eb95871b47bac0
Dominik Schmidtlein <dominik.schmidtlein@gmail.com>
2017-11-04 20:53:22 -0400

    added tests for models

--- test/models/desk_test.rb
+++ test/models/desk_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class DeskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @desk1 = desks :desk1
+  end
+
+  test 'valid' do
+    assert @desk1.valid?
+  end
+
+  test 'invalid without name' do
+    @desk1.name = nil
+    assert_not @desk1.valid?
+    assert @desk1.errors.key? :name
+  end
+
+  test 'invalid without grouping' do
+    @desk1.grouping = nil
+    assert_not @desk1.valid?
+    assert @desk1.errors.key? :grouping
+  end
+
+  test 'invalid without occupied' do
+    @desk1.occupied = nil
+    assert_not @desk1.valid?
+    assert @desk1.errors.key? :occupied
+  end
+
+  test 'belongs to grouping1' do
+    assert_equal(groupings(:grouping1), @desk1.grouping)
+  end
 end
