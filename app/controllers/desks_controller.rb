c0df008a4f5033e0490c8f217039e4bd0ec53d92
erickadbay <erickadbay@gmail.com>
2017-11-05 22:55:49 -0500

    Added index, create, update, show endpoints and updated UI

---
+++ app/controllers/desks_controller.rb
@@ -0,0 +1,41 @@
+class DesksController < ApplicationController
+  before_action :load_desk, only: %i(show update)
+
+  respond_to :html, only: %i(show)
+  respond_to :json, only: %i(show create update)
+
+  def index
+    @desks = Desk.all
+  end
+
+  def create
+    desk = Desk.new(desk_params)
+    if desk.save
+      respond_with(desk)
+    else
+      render_exception(desk)
+    end
+  end
+
+  def show
+    respond_with(@desk)
+  end
+
+  def update
+    if @desk.update(desk_params)
+      respond_with(@desk)
+    else
+      render_exception(@desk)
+    end
+  end
+
+  private
+
+  def load_desk
+    @desk = Desk.find(params[:id])
+  end
+
+  def desk_params
+    params.require(:desk).permit(:name, :occupied)
+  end
+end
---
+++ test/controllers/desks_controller_test.rb
@@ -0,0 +1,55 @@
+require 'test_helper'
+
+class DesksControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @desk = desks(:desk1)
+  end
+
+  test "#index responds with all the desks" do
+    get desks_path
+
+    assert_response :success
+  end
+
+  test "#create responds with 200 if params are valid" do
+    post desks_path(format: :json), params: { desk: { name: 'test desk' } }
+
+    parsed_response = JSON.parse(response.body, symbolize_names: true)
+
+    assert_response :success
+    refute_nil parsed_response[:id]
+    assert_equal 'test desk', parsed_response[:name]
+  end
+
+  test "#create responds with 422 if name is null" do
+    post desks_path, params: { desk: { name: nil } }
+
+    assert_response :unprocessable_entity
+  end
+
+  test "#show responds with desk" do
+    get desk_path(@desk)
+
+    assert_response :success
+    assert_template 'show'
+  end
+
+  test "#show responds with desk in json format" do
+    get desk_path(@desk, format: :json)
+
+    assert_response :success
+    assert_equal @desk.to_json, response.body
+  end
+
+  test "#update updates desk and responds with updated desk" do
+    put desk_path(@desk, format: :json), params: { desk: { occupied: false } }
+
+    assert_response 204
+  end
+
+  test "#update responds with 422 if occupied is attribute is null" do
+    put desk_path(@desk, format: :json), params: { desk: { name: nil } }
+
+    assert_response :unprocessable_entity
+  end
+end
