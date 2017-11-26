24683c2b123aa251b5cbe7d88c34444001f86c3f
erickadbay <erickadbay@gmail.com>
2017-11-26 17:06:15 -0500

    Redis configuration for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,8 +41,9 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = 'wss://smart-library-app.herokuapp.com/cable'
+  config.web_socket_server_url = 'wss://smart-library-app.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = ['http://smart-library-app.herokuapp.com', 'https://smart-library-app.herokuapp.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
