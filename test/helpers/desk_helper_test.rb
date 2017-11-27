5c73090ca5c9a42c3fe4e888141c35725cabe3c9
Michael Vezina <Vezina95@bell.net>
2017-11-27 14:29:01 -0500

    - Updated desk names in desks fixture - Styling nit accommodations for Erick

--- test/helpers/desk_helper_test.rb
+++ test/helpers/desk_helper_test.rb
@@ -8,17 +8,20 @@ class DeskHelperTest < ActionView::TestCase
   end
 
   test '#desk_status gives correct status' do
-    assert_equal(desk_status(@occupied_desk), 'Occupied')
-    assert_equal(desk_status(@unoccupied_desk), 'Not Occupied')
+    assert_equal desk_status(@occupied_desk), 'Occupied'
+    assert_equal desk_status(@unoccupied_desk), 'Not Occupied'
   end
 
   test '#desk_class gives correct class' do
-    assert_equal(desk_class(@occupied_desk), 'danger')
-    assert_equal(desk_class(@unoccupied_desk), 'success')
+    assert_equal desk_class(@occupied_desk), 'danger'
+    assert_equal desk_class(@unoccupied_desk), 'success'
   end
 
   test '#desk_element_id gives correct element id' do
-    assert_equal(desk_element_id(@occupied_desk), "desk_state_#{@occupied_desk.id}")
-    assert_equal(desk_element_id(@unoccupied_desk), "desk_state_#{@unoccupied_desk.id}")
+    assert_equal desk_element_id(@occupied_desk),
+                 "desk_state_#{@occupied_desk.id}"
+
+    assert_equal desk_element_id(@unoccupied_desk),
+                 "desk_state_#{@unoccupied_desk.id}"
   end
 end
