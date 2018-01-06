4479a095a0fcc7ccaefae5290048244e6a576aca
Dominik Schmidtlein <dominik.schmidtlein@gmail.com>
2018-01-05 23:38:39 -0500

    allowed uploading background images for layouts

--- app/models/grouping.rb
+++ app/models/grouping.rb
@@ -4,7 +4,9 @@ class Grouping < ApplicationRecord
   belongs_to :parent_grouping, class_name: 'Grouping', optional: true
   has_many :desks
   belongs_to :coordinate, optional: true
+  has_attached_file :background
 
+  do_not_validate_attachment_file_type :background
   validates :name, presence: true
 
   def children
