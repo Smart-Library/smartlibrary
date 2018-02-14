3fb13c6165bb79533f57751d7948731539ca515c
Dominik Schmidtlein <dominik.schmidtlein@gmail.com>
2018-02-14 09:44:47 -0500

    broacast to socket in after_save callback instead of controllers (#29)

--- test/models/desk_test.rb
+++ test/models/desk_test.rb
@@ -1,31 +1,52 @@
 require 'test_helper'
 
 class DeskTest < ActiveSupport::TestCase
-  def setup
+  include ActionCable::TestHelper
+
+  setup do
     @desk1 = desks :test_desk
   end
 
-  test 'valid' do
+  test "valid" do
     assert @desk1.valid?
   end
 
-  test 'valid without grouping' do
+  test "valid without grouping" do
     @desk1.grouping = nil
     assert_predicate @desk1, :valid?
   end
 
-  test 'invalid without name' do
+  test "invalid without name" do
     @desk1.name = nil
     refute_predicate @desk1, :valid?
     assert @desk1.errors.key? :name
   end
 
-  test 'occupied defaults to false' do
+  test "occupied defaults to false" do
     desk = Desk.new
     assert_equal(false, desk.occupied)
   end
 
-  test 'belongs to grouping1' do
+  test "belongs to grouping1" do
     assert_equal(groupings(:grouping1), @desk1.grouping)
+  end
+
+  test "broadcast when change to occupied" do
+    status = @desk1.occupied?
+    @desk1.update(occupied: !status)
+
+    assert_broadcast_on 'desks', desk_info: { id: @desk1.id, occupied: !status }
+  end
+
+  test "no broadcast when no change to occupied" do
+    @desk1.update(occupied: @desk1.occupied?)
+
+    assert_no_broadcasts 'desks'
+  end
+
+  test "no broadcast when change to name" do
+    @desk1.update(name: 'Random new name')
+
+    assert_no_broadcasts 'desks'
   end
 end
