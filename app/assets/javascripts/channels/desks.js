3517f308b85186f623ad82d628b87452e7f70458
Dominik Schmidtlein <dominik.schmidtlein@gmail.com>
2018-03-19 16:24:30 -0400

    update ui from broadcast (#34)

--- app/assets/javascripts/channels/desks.js
+++ app/assets/javascripts/channels/desks.js
@@ -10,12 +10,10 @@ App.cable = ActionCable.createConsumer();
 
 App.messages = App.cable.subscriptions.create('DesksChannel', {
   received: function(data) {
-      // Get desk state button
-      var target = $("#desk_state_" + data.desk_info.id)[0];
+    // Get desk state button
+    var target = $("#desk_state_" + data.desk_info.id)[0];
 
-      if(!target)
-          return;
-
+    if (target) {
       // Remove any pre-existing occupied/not_occupied classes before setting new class
       target.classList.remove('btn-outline-success');
       target.classList.remove('btn-outline-danger');
@@ -23,5 +21,13 @@ App.messages = App.cable.subscriptions.create('DesksChannel', {
       // Change button based on whether desk is occupied
       target.classList.add(data.desk_info.occupied ? 'btn-outline-danger' : 'btn-outline-success');
       target.innerText = data.desk_info.occupied ? 'Occupied' : 'Not Occupied';
+    }
+    // groupings
+    target = $('.desk[data-id="' + data.desk_info.id + '"]')[0];
+    if (target) {
+      target.classList.remove('occupied');
+      target.classList.remove('free');
+      target.classList.add(data.desk_info.occupied ? 'occupied' : 'free');
+    }
   }
 });
